19dde0254f33bf7b3ab5fe71f8c3cbdbdc70732c
Unknown <noenavarro2@hotmail.com>
2018-01-18 09:22:04 +0100

    primer modulo configurado
    primeros pasos modulo scafford

---
+++ models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
